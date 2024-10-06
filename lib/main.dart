6f121c66e859e32d31544a7d1b772016da41a7b5
Omar Khaled Essam <omar.noureldin800@gmail.com>
2024-10-06 20:08:03 +0300

    Added the 2 teams with their buttons and score

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,154 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyWidget());
+  runApp(PointsCounter());
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
+class PointsCounter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hi'),
+          backgroundColor: Colors.orange,
+          title: Text('Points Counter'),
+        ),
+        body: Column(
+          children: [
+            IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        'Team A',
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        '0',
+                        style: TextStyle(fontSize: 200),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.orange,
+                            minimumSize: Size(150, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            )),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 1 Point',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.orange,
+                            minimumSize: Size(150, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            )),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 2 Point',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.orange,
+                            minimumSize: Size(150, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            )),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 3 Point',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  VerticalDivider(
+                    indent: 30,
+                    color: Colors.grey,
+                    thickness: 2,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Team B',
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        '0',
+                        style: TextStyle(fontSize: 200),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.orange,
+                            minimumSize: Size(150, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            )),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 1 Point',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.orange,
+                            minimumSize: Size(150, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            )),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 2 Point',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Colors.orange,
+                            minimumSize: Size(150, 40),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            )),
+                        onPressed: () {},
+                        child: Text(
+                          'Add 3 Point',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
